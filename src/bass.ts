231af67875f3378f361794140c39181f66c9f0f1
busa-robi <busarobertdavid@gmial.com>
2025-12-15 00:14:45 +0100

    uj ts-ek + lekerdezes

---
+++ src/bass.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+const Adiv : HTMLDivElement = document.querySelector('#A') as HTMLDivElement;
+const Bdiv : HTMLDivElement = document.querySelector('#B') as HTMLDivElement;
+const Cdiv : HTMLDivElement = document.querySelector('#C') as HTMLDivElement;
+const Ddiv : HTMLDivElement = document.querySelector('#D') as HTMLDivElement;
+const Ediv : HTMLDivElement = document.querySelector('#E') as HTMLDivElement;
+const Fdiv : HTMLDivElement = document.querySelector('#F') as HTMLDivElement;
+const Gdiv : HTMLDivElement = document.querySelector('#G') as HTMLDivElement;
---
+++ src/drums.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+const CCdiv : HTMLDivElement = document.querySelector('#CC') as HTMLDivElement;
+const RCdiv : HTMLDivElement = document.querySelector('#RC') as HTMLDivElement;
+const MTdiv : HTMLDivElement = document.querySelector('#MT') as HTMLDivElement;
+const SDdiv : HTMLDivElement = document.querySelector('#SD') as HTMLDivElement;
+const FTdiv : HTMLDivElement = document.querySelector('#FT') as HTMLDivElement;
+const BDdiv : HTMLDivElement = document.querySelector('#BD') as HTMLDivElement;
+const HHdiv : HTMLDivElement = document.querySelector('#HH') as HTMLDivElement;
