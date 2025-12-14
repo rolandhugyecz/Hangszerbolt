3f7dc4745231ad1d74cc3881aac51e9391980e37
rolandhugyecz <hurolika@gmail.com>
2025-12-14 19:39:04 +0100

    Sounds, guitar.html, guitar.ts

---
+++ src/guitar.ts
@@ -0,0 +1,44 @@
+import './style.css'
+
+const ADiv : HTMLDivElement = document.querySelector('#Achord') as HTMLDivElement;
+const EDiv : HTMLDivElement = document.querySelector('#Echord') as HTMLDivElement;
+const Ddiv : HTMLDivElement = document.querySelector('#Dchord') as HTMLDivElement;
+const GDiv : HTMLDivElement = document.querySelector('#Gchord') as HTMLDivElement;
+const BDiv : HTMLDivElement = document.querySelector('#Bchord') as HTMLDivElement;
+const CDiv : HTMLDivElement = document.querySelector('#Cchord') as HTMLDivElement;
+const ASND = new Audio(new URL('./sounds/guitar/A.m4a', import.meta.url).href);
+const BSND = new Audio(new URL('./sounds/guitar/B.m4a', import.meta.url).href);
+const CSND = new Audio(new URL('./sounds/guitar/C.m4a', import.meta.url).href);
+const DSND = new Audio(new URL('./sounds/guitar/D.m4a', import.meta.url).href);
+const ESND = new Audio(new URL('./sounds/guitar/E.m4a', import.meta.url).href);
+const GSND = new Audio(new URL('./sounds/guitar/G.m4a', import.meta.url).href);
+
+ADiv.addEventListener('click', ()=>{
+    ASND.currentTime = 0;
+    ASND.play();
+})
+
+BDiv.addEventListener('click', ()=>{
+    BSND.currentTime = 0;
+    BSND.play();
+})
+
+CDiv.addEventListener('click', ()=>{
+    CSND.currentTime = 0;
+    CSND.play();
+})
+
+Ddiv.addEventListener('click', ()=>{
+    DSND.currentTime = 0;
+    DSND.play();
+})
+
+EDiv.addEventListener('click', ()=>{
+    ESND.currentTime = 0;
+    ESND.play();
+})
+
+GDiv.addEventListener('click', ()=>{
+    GSND.currentTime = 0;
+    GSND.play();
+})
